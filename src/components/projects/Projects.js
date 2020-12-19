d3c6cae2f7a9c8d5c0df3b13d40e3b9494d4fc16
Cory Scharf <cscharf91@gmail.com>
2020-12-19 17:06:16 -0500

    added creddit project to featured project

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,7 +3,8 @@ import Nav from '../Nav';
 import TradeTracker from '../../images/trade-tracker-preview.png';
 import RJS from '../../images/rjs-billing.png';
 import FBClone from '../../images/fb-clone.png';
-import Chess from '../../images/chess.png';
+import Creddit from '../../images/creddit-screen1.png';
+import Creddit2 from '../../images/creddit-screen2.png';
 import './Projects.css';
 
 const Projects = () => {
@@ -13,12 +14,33 @@ const Projects = () => {
         <Nav title="Projects" />
         <div className="projects-section">
             <div className="project-container">
+                <p className="project-title">Creddit:</p>
+                <a href="https://repl.it/@Cscharf91/chess" rel="noopener noreferrer" target="_blank"><img src={Creddit} alt="Creddit preview 1" className="project-pic" /></a><br /><br />
+                <a href="https://repl.it/@Cscharf91/chess" rel="noopener noreferrer" target="_blank"><img src={Creddit2} alt="Creddit preview 2" className="project-pic" /></a><br />
+                <div className="project-desc">
+                    <p className="project-bio">Build with Node.js, Express, MongoDB, and React. A website to share photos and have discussions, and vote on the best content!</p>
+                    <p>Features:</p>
+                    <ul>
+                        <li>Create Zones (akin to Subreddits): a little section of Creddit to discuss any topic or hobby (ex: pics, pets, fitness, gaming)</li>
+                        <li>Upvote and downvote comments</li>
+                        <li>Nested comments</li>
+                        <li>Optional image upload on posts</li>
+                        <li>User profiles</li>
+                        <li>Responsive design</li>
+                    </ul>
+                </div>
+                <div className="btn-container">
+                    <a className="nav-btn-inverted" href="https://powerful-chamber-15419.herokuapp.com/" rel="noopener noreferrer" target="_blank">View Website</a>
+                    <a className="nav-btn-inverted" href="https://github.com/Cscharf91/stock-trade-tracker" rel="noopener noreferrer" target="_blank">View Code</a> 
+                </div>
+            </div>
+            <div className="project-container">
                 <div className="project-title-wrapper">
                   <p className="project-title">Trade Tracker:</p>
                 </div>
                 <a href="https://powerful-chamber-15419.herokuapp.com/" rel="noopener noreferrer" target="_blank"><img src={TradeTracker} alt="Trade Tracker" className="project-pic" /></a><br />
                 <div className="project-desc">
-                    <p className="project-bio">Built in Rails and React. Enter stock trade data to see how different strategies are performing. The filters are based on my own experience trading.</p>
+                    <p className="project-bio">Built with Rails and React. Enter stock trade data to see how different strategies are performing. The filters are based on my own experience trading.</p>
                     <p>Features:</p>
                     <ul>
                         <li>User authorization with Devise, keeping each user's trades private</li>
@@ -68,17 +90,6 @@ const Projects = () => {
                 <div className="btn-container">
                     <a className="nav-btn-inverted" href="https://fast-sierra-54264.herokuapp.com/" rel="noopener noreferrer" target="_blank">View Website</a>
                     <a className="nav-btn-inverted" href="https://github.com/Cscharf91/facebook-clone" rel="noopener noreferrer" target="_blank">View Code</a> 
-                </div>
-            </div>
-            <div className="project-container">
-                <p className="project-title">Chess:</p>
-                <a href="https://repl.it/@Cscharf91/chess" rel="noopener noreferrer" target="_blank"><img src={Chess} alt="Chess" className="project-pic" /></a><br />
-                <div className="project-desc">
-                    <p className="project-bio">The classic game, made in Ruby to play in the command line. Save and load your games at any point with serialization!</p>
-                </div>
-                <div className="btn-container">
-                    <a className="nav-btn-inverted" href="https://repl.it/@Cscharf91/chess" rel="noopener noreferrer" target="_blank">View Repl.it</a>
-                    <a className="nav-btn-inverted" href="https://github.com/Cscharf91/chess" rel="noopener noreferrer" target="_blank">View Code</a> 
                 </div>
             </div>
         </div>
